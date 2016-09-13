8fc75f8713feeb6df2ad8defe3a0d67c97e0bc5a
jastrelax <yinpinjiu@gmail.com>
2016-09-13 17:45:00 +0800

    更换实现思路

--- app/src/main/java/com/hyc/eyepetizer/view/PgcActivity.java
+++ app/src/main/java/com/hyc/eyepetizer/view/PgcActivity.java
@@ -1,7 +1,23 @@
 package com.hyc.eyepetizer.view;
 
+import android.animation.Animator;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+
 import com.hyc.eyepetizer.R;
 import com.hyc.eyepetizer.base.BaseActivity;
+import com.hyc.eyepetizer.model.FromType;
+import com.hyc.eyepetizer.view.adapter.FragmentAdapter;
+import com.hyc.eyepetizer.view.fragment.VideoListFragment;
+import com.hyc.eyepetizer.widget.MyAnimatorListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
 
 /**
  * Created by ray on 16/9/13.
@@ -15,5 +31,52 @@ public class PgcActivity extends BaseActivity {
 
     @Override protected int getLayoutID() {
         return R.layout.activity_new;
+    }
+    private AccelerateDecelerateInterpolator mInterpolator = new AccelerateDecelerateInterpolator();
+    private MyAnimatorListener mListener = new MyAnimatorListener() {
+        @Override
+        public void onAnimationEnd(Animator animator) {
+        }
+    };
+    private VideoListFragment mWeek;
+    private VideoListFragment mMonth;
+    private VideoListFragment mHistory;
+    private FragmentAdapter mAdapter;
+    private List<VideoListFragment> mFragments;
+    ViewPager vpVideo;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        vpVideo= (ViewPager) findViewById(R.id.vp_target);
+        mFragments = new ArrayList<>();
+        mWeek = VideoListFragment.instantiate(FromType.TYPE_WEEK, FromType.Tag.RANK_WEEK);
+        mMonth = VideoListFragment.instantiate(FromType.TYPE_MONTH, FromType.Tag.RANK_MONTH);
+        mHistory = VideoListFragment.instantiate(FromType.TYPE_HISTORY, FromType.Tag.RANK_HISTORY);
+        mFragments.add(mWeek);
+//        mFragments.add(mMonth);
+//        mFragments.add(mHistory);
+        mAdapter = new FragmentAdapter(getSupportFragmentManager(), mFragments);
+        vpVideo.setAdapter(mAdapter);
+        vpVideo.setOffscreenPageLimit(2);
+//        vpVideo.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+//            @Override
+//            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+//                if (preIndex > position) {
+//                    indicator.setX(((positionOffset - 1) * mIndicatorScroll) + mIndicatorScroll * preIndex+mIndicatorY[0]);
+//                } else if (preIndex <= position) {
+//                    indicator.setX(positionOffset * mIndicatorScroll + mIndicatorScroll * preIndex+mIndicatorY[0]);
+//                }
+//            }
+//
+//            @Override
+//            public void onPageSelected(int position) {
+//                preIndex=position;
+//            }
+//
+//            @Override
+//            public void onPageScrollStateChanged(int state) {
+//
+//            }
+//        });
     }
 }
--- app/src/main/java/com/hyc/eyepetizer/widget/PullToScrollView.java
+++ app/src/main/java/com/hyc/eyepetizer/widget/PullToScrollView.java
@@ -2,21 +2,36 @@ package com.hyc.eyepetizer.widget;
 
 import android.content.Context;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.widget.FrameLayout;
+import android.widget.HorizontalScrollView;
+import android.widget.ScrollView;
+import android.widget.Scroller;
+
 import com.hyc.eyepetizer.R;
+import com.hyc.eyepetizer.utils.AppUtil;
 
 /**
  * Created by ray on 16/9/12.
  */
-public class PullToScrollView extends FrameLayout {
+public class PullToScrollView extends ScrollView {
     private int mHeight;
     private int mWidth;
     private View head;
     private View indicator;
     private ViewPager pager;
+    private float mTitleBarHeight;
+    private int mHintY;
+    private GestureDetector mGestureDetector;
+    private Scroller mScroller;
+
 
 
     public PullToScrollView(Context context) {
@@ -31,36 +46,104 @@ public class PullToScrollView extends FrameLayout {
 
     public PullToScrollView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-
-        ;
+        mTitleBarHeight = AppUtil.dip2px(45);
+//        mGestureDetector=new GestureDetector(getContext(),new GestureListener());
+//        mScroller = new Scroller(getContext());
+        mMaximumVelocity = ViewConfiguration.get(context).getScaledMaximumFlingVelocity();
     }
 
+    private float mCurrentY;
+    int mPointerId;
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        float y = event.getRawY();
+        int scrollY = getScrollY();
+        boolean intercept = false;
+        if (scrollY < mHintY) {
+            intercept = true;
+        }
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mPointerId = event.getPointerId(0);
 
-    @Override protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+                mVelocityTracker = VelocityTracker.obtain(); // 初始化
+                mVelocityTracker.addMovement(event);
+                mCurrentY = y;
+            case MotionEvent.ACTION_MOVE:
+
+                if (intercept) {
+                    if (scrollY == 0 && y > mCurrentY) {
+                        mCurrentY = y;
+                        return super.dispatchTouchEvent(event);
+                    } else if (scrollY+mCurrentY-y<0) {
+                        mCurrentY=y;
+                        scrollTo(0,0);
+                        return true;
+                    } else if (mCurrentY - y + scrollY > mHintY) {
+                        scrollBy(0, mHintY - scrollY);
+                    } else {
+                        scrollBy(0, (int) (mCurrentY - y));
+                    }
+                    mVelocityTracker.computeCurrentVelocity(1000, mMaximumVelocity);
+                    int initialVelocity = (int) mVelocityTracker.getYVelocity(mPointerId);
+                    // 在 ACTION_UP 和 ACTION_CANCEL 中 销毁
+                    if (initialVelocity!=0) {
+                        fling(initialVelocity);
+                    }
+
+                    mCurrentY = y;
+                    return true;
+                }
+                break;
+            //https://github.com/hyr0318/MaterialCoordinatorLayout
+            case MotionEvent.ACTION_UP:
+                mVelocityTracker.computeCurrentVelocity(1000, mMaximumVelocity);
+                int initialVelocity = (int) mVelocityTracker.getYVelocity(mPointerId);
+                mVelocityTracker.recycle();
+                Log.e("initialVelocity",initialVelocity+"----");
+                // 在 ACTION_UP 和 ACTION_CANCEL 中 销毁
+                fling(initialVelocity);
+                break;
+        }
+        return super.dispatchTouchEvent(event);
+    }
+    VelocityTracker mVelocityTracker;
+    int mMaximumVelocity ;
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        mHeight = getMeasuredHeight();
-        mWidth = getMeasuredWidth();
+        mHintY = head.getMeasuredHeight() - indicator.getMeasuredHeight();
+        int width=  MeasureSpec.getSize(widthMeasureSpec);
+        int height=MeasureSpec.getSize(heightMeasureSpec);
+        mHeight = height;
+        mWidth = width;
         //FrameLayout.LayoutParams params= (LayoutParams) pager.getLayoutParams();
         //params.width=mWidth;
         //params.height=mHeight-mHeight-head.getMeasuredHeight();
-
-        measureChild(pager, MeasureSpec.makeMeasureSpec(mWidth, MeasureSpec.AT_MOST),
-            MeasureSpec.makeMeasureSpec(mHeight - indicator.getMeasuredHeight(),
-                MeasureSpec.AT_MOST));
+        setMeasuredDimension(width,height);
+//        measureChild(all,MeasureSpec.makeMeasureSpec(mWidth, MeasureSpec.AT_MOST),
+//                MeasureSpec.makeMeasureSpec(height+mHintY,
+//                        MeasureSpec.AT_MOST));
+        all.measure(MeasureSpec.makeMeasureSpec(mWidth, MeasureSpec.AT_MOST),
+                MeasureSpec.makeMeasureSpec(height+mHintY,
+                        MeasureSpec.AT_MOST));
+        pager.measure(MeasureSpec.makeMeasureSpec(mWidth, MeasureSpec.AT_MOST),
+                MeasureSpec.makeMeasureSpec(height - indicator.getMeasuredHeight(),
+                        MeasureSpec.AT_MOST));
     }
 
 
-    @Override protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
         super.onLayout(changed, left, top, right, bottom);
-
         pager.layout(0, head.getMeasuredHeight(), mWidth,
-            (mHeight - indicator.getMeasuredHeight() + head.getMeasuredHeight()));
-
-        pager.postDelayed(new Runnable() {
-            @Override public void run() {
-                scrollTo(0, head.getMeasuredHeight() - indicator.getMeasuredHeight());
-            }
-        }, 1000);
+                (mHeight + mHintY));
+//
+//        pager.postDelayed(new Runnable() {
+//            @Override public void run() {
+//                scrollTo(0, mHintY);
+//            }
+//        }, 1000);
         Log.e("hyc-test", "head--" + head.getMeasuredWidth() + "---" + head.getMeasuredHeight());
         Log.e("hyc-test", "pager--" + pager.getMeasuredWidth() + "---" + pager.getMeasuredHeight());
         Log.e("hyc-test", "pageall--" + mWidth + "---" + mHeight);
@@ -68,7 +151,8 @@ public class PullToScrollView extends FrameLayout {
     }
 
 
-    @Override protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
         mHeight = h;
         mWidth = w;
@@ -77,11 +161,65 @@ public class PullToScrollView extends FrameLayout {
     }
 
 
-    @Override protected void onFinishInflate() {
+    @Override
+    protected void onFinishInflate() {
         super.onFinishInflate();
         head = findViewById(R.id.rl_head);
         indicator = findViewById(R.id.rl_indicator);
         pager = (ViewPager) findViewById(R.id.vp_target);
-
+        all=findViewById(R.id.fl_all);
     }
+    private View all;
+//    public class GestureListener extends GestureDetector.SimpleOnGestureListener {
+//
+//        @Override
+//        public boolean onDown(MotionEvent motionEvent) {
+//            Log.e("--GestureListener--","onDown");
+//            return true;
+//        }
+//
+//        @Override
+//        public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+//            Log.e("--GestureListener--","onScroll");
+//            int scrollY = getScrollY();
+//            boolean intercept = true;
+//            if (scrollY > mHintY) {
+//               return false;
+//            } else if (getScrollY() > mTitleBarHeight) {
+//
+//            }
+//            Log.e("--motion--",v1+"+++1++"+motionEvent.getRawY());
+//            float dY=-v1;
+//            Log.e("---",dY+"-");
+//            if (scrollY == 0 && dY>0) {
+//                mCurrentY=motionEvent1.getRawY();
+//                        return false;
+//                    } else if (scrollY-dY<0) {
+//                        mCurrentY=motionEvent1.getRawY();
+//                        scrollTo(0,0);
+//                        return true;
+//                    } else if (scrollY-dY > mHintY) {
+//                        scrollBy(0, mHintY - scrollY);
+//                    } else {
+//                        scrollBy(0, (int) (-dY));
+//                    }
+//            return false;
+//        }
+//        @Override
+//        public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+//            Log.e("--GestureListener--","onFling");
+//
+//            mScroller.fling(0,getScrollY(),0, -(int) v1*3,0,0,0,mHintY);
+//            return false;
+//        }
+//    }
+//    @Override
+//    public void computeScroll() {
+//        if (mScroller.computeScrollOffset()) {
+//            scrollTo(mScroller.getCurrX(), mScroller.getCurrY());
+//            postInvalidate();
+//        }
+//        super.computeScroll();
+//    }
+
 }

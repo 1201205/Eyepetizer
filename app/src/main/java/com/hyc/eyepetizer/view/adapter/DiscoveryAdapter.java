f95b6a0b5084972663e7990f11c8c102d82a62a0
1201205 <534012134@qq.com>
2016-09-23 00:41:58 +0800

    添加loopAdapter

---
+++ app/src/main/java/com/hyc/eyepetizer/view/adapter/DiscoveryAdapter.java
@@ -0,0 +1,44 @@
+package com.hyc.eyepetizer.view.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+import com.hyc.eyepetizer.model.beans.ViewData;
+import com.hyc.eyepetizer.utils.WidgetHelper;
+import java.util.List;
+
+/**
+ * Created by ray on 16/9/22.
+ */
+public class DiscoveryAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private List<ViewData> mDatas;
+    private Context mContext;
+
+
+    public DiscoveryAdapter(Context context, List<ViewData> datas) {
+        mDatas = datas;
+        mContext = context;
+    }
+
+
+    @Override public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+
+    @Override public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+
+    @Override public int getItemCount() {
+        return mDatas == null ? 0 : mDatas.size();
+    }
+
+
+    @Override
+    public int getItemViewType(int position) {
+        return (WidgetHelper.getViewType(mDatas.get(position).getType()));
+    }
+
+}
